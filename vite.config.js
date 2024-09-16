9746fdd3371dc2173337756508c7da75a46dc9f1
Emmanuel William <emmywilly1@gmail.com>
2024-09-16 07:07:13 +0100

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/zillionsolution-vite/', // Your repo name here
   plugins: [react()],
-})
+});
+
